381868272a2fd74141499d91a32cc31f27e08cf7
jojo626262 <deniz.akdeniz2021@gmail.com>
2026-09-16 13:33:35 +0200

    Add cell creation logic and implement renderRow function for game board

--- js/board.js
+++ js/board.js
@@ -1,5 +1,6 @@
 const board = document.createElement("div");
 board.classList.add("board");
+const cells = [];
 
 for(let row = 0; row < 6; row++){
     const rowDiv = document.createElement("div");
@@ -9,8 +10,18 @@ for(let row = 0; row < 6; row++){
         const cellDiv = document.createElement("div");
         cellDiv.classList.add("cell");
         rowDiv.appendChild(cellDiv);
+        cells.push(cellDiv);
     }
     board.appendChild(rowDiv);
 }
 
 document.getElementById("board-container").appendChild(board);
+
+
+ function renderRow(rowIndex, word, results){
+    for(let col = 0; col < 5; col++){
+        const cell = cells[rowIndex * 5 + col];
+        cell.textContent = word[col];
+        cell.classList.add(results[col]); 
+    }
+}
